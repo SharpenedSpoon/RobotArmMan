bffd9cba5168c5210c3604ef9e9228dd0ec48896
Ian Douglas <iandouglas@gmail.com>
2013-12-01 17:03:49 -0700

    Some animations

--- Assets/_Subfacet/Scripts/PlayerCharacter.cs
+++ Assets/_Subfacet/Scripts/PlayerCharacter.cs
@@ -7,17 +7,23 @@ public class PlayerCharacter : MonoBehaviour {
 	private Health _health = null;
 	private Shooter _shooter = null;
 
+	private Animator _anim;
+
+
 	void Start () {
 		_movement = GetComponent<Movement>();
 		_health = GetComponent<Health>();
 		_shooter = GetComponent<Shooter>();
-		if (_movement == null || _health == null || _shooter == null) {
+		_anim = GetComponent<Animator>();
+		if (_movement == null || _health == null || _shooter == null || _anim == null) {
 			Debug.Log("WTF bro? Add all components first. - Hugs and Kisses, " + gameObject.name);
 			Destroy(gameObject);
 		}
 	}
 
 	void Update () {
+		_anim.SetBool("Running", false);
+		_anim.SetBool("Shooting", false);
 		// ----- Get the input for movement
 		int hor = 0;
 		int ver = 0;
@@ -28,11 +34,20 @@ public class PlayerCharacter : MonoBehaviour {
 
 		// ----- Apply movement
 		_movement.Move(hor);
+		if (hor != 0) {
+			_anim.SetBool("Running", true);
+		}
 		transform.position += ver * _movement.Speed * Time.deltaTime * transform.up; // vertical movement for debugging
 
 		// ----- Shoot stuff
 		if (Input.GetKey(KeyCode.Z)) {
 			_shooter.Shoot(_movement.Direction);
+			_anim.SetBool("Shooting", true);
 		}
+	}
+
+	public void HandleAnimation() {
+		// ----- Basic animation handling. Let's do this better later.
+
 	}
 }
