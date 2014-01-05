ce977c65f42655516a89f0bfa143073b0d60c8c9
Ian Douglas <iandouglas@gmail.com>
2014-01-05 15:12:04 -0700

    Doors and objects that unlock upon object(s) death

---
+++ Assets/LockUntilObjectDeath.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class LockUntilObjectDeath : MonoBehaviour {
+
+	public List<GameObject> lockingObjects;
+
+	void Update () {
+
+		// ----- Remove dead objects
+		lockingObjects.RemoveAll(item => item == null);
+
+		// ----- Open if all objects are dead
+		if (lockingObjects.Count == 0) {
+			Destroy(gameObject);
+		}
+	}
+}
